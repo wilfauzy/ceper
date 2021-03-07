924fb146733459c4cc157e43f12d513cdc011c3e
Wildan Fauzy <me@wildanfauzy.com>
2021-03-07 22:20:05 +0700

    update website-config.ts

--- src/website-config.ts
+++ src/website-config.ts
@@ -49,16 +49,16 @@ export interface WebsiteConfig {
 }
 
 const config: WebsiteConfig = {
-  title: 'Ghost',
-  description: 'The professional publishing platform',
+  title: 'Blog Idan',
+  description: 'Jernih dan Mengalir Membawa pesan',
   coverImage: 'img/blog-cover.png',
   logo: 'img/ghost-logo.png',
   lang: 'en',
-  siteUrl: 'https://gatsby-casper.netlify.com',
-  facebook: 'https://www.facebook.com/ghost',
-  twitter: 'https://twitter.com/tryghost',
+  siteUrl: 'https://blog.idan.my.id',
+  facebook: 'https://www.facebook.com/wilfauzy',
+  twitter: 'https://twitter.com/wilfauzy',
   showSubscribe: true,
-  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpAction: 'https://wildanfauzy.us10.list-manage.com/subscribe/post?u=72d29580a7ee388212e8622a5&id=f06aca7400',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
